e37b74321a473809f700631b277c1c94d06c4090
Pivi Lartisant <paul.lartisant@icloud.com>
2022-11-10 10:15:34 +0100

    Initial upload (no assets)

---
+++ prototype/scripts/audio.js
@@ -0,0 +1,12 @@
+// for legacy browsers
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+const audioContext = new AudioContext();
+
+// get the audio element
+let audioElement = document.querySelectorAll("audio");
+
+console.log(audioElement)
+
+// pass it into the audio context
+// const track = audioContext.createMediaElementSource(audioElement);
